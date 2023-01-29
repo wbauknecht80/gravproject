c75697a3bb07e12d798b36213497c91d83507399
wbauknecht80 <wbauknecht80@gmail.com>
2023-01-29 16:51:28 -0600

    Implement the beginnings of the javascript. I am forseeing some issues with implemeting the mass inputs. Need to figure out to to ensure selecting a button for mass one does not input to mass two and vice versa.

---
+++ script.js
@@ -0,0 +1,13 @@
+const inputButtons = document.querySelectorAll('button');
+
+const humanInput = document.getElementById('human');
+const carInput = document.getElementById('car');
+const buildingInput = document.getElementById('building');
+const earthInput = document.getElementById('earth');
+
+const millimeter = document.getElementById('millimeter');
+const meter = document.getElementById('meter');
+const kilometer = document.getElementById('kilometer');
+const au = document.getElementById('au');
+const lightyear =  document.getElementById('lightyear');
+
